9bde0bb73593576980df9fac57d2e72f3ee1ebaa
nn4343 <gea01441@nifty.com>
2016-11-12 21:34:46 +0900

    変更

---
+++ SLAG/slagUnityProject/m3/Assets/test.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+public class test : MonoBehaviour
+{
+    //Server m_server;
+    //void Start()
+    //{
+    //    m_server = new Server();
+    //    m_server.Run();
+    //}
+
+    //void Update()
+    //{
+    //    if (m_server==null) return;
+
+    //    if (m_server.m_req.HasMsg())
+    //    {
+    //        var req = m_server.m_req.GetMsg();
+    //        m_server.m_res.SetMsg("OK!" + Time.time);
+    //    }
+    //}
+
+    //public void OnDestroy()
+    //{
+    //    m_server.Stop();
+    //}
+
+    TcpServer m_server;
+    IEnumerator Start()
+    {
+        m_server = new TcpServer();
+        m_server.Run();
+
+        yield return null;
+    }
+
+
+}
