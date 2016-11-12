99e34119efaab09b928643094fb9ccde14583113
nn4343 <gea01441@nifty.com>
2016-11-12 23:15:20 +0900

    pipe成功した。 次の段階へ

---
+++ SLAG/slagUnityProject/m5/TcpPipeClient/TcpPipeClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TcpPipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipe = new TcpPipe("127.0.0.1",2002);
+            pipe.Start(s=>Console.WriteLine(s));
+
+            while(true)
+            {
+                string readmsg =null;
+                while(true)
+                {
+                    var s = pipe.Read();
+                    if (s==null) break;
+                    readmsg += s + Environment.NewLine;
+                }
+                Console.WriteLine(readmsg);
+
+                pipe.Update();
+                Console.Write("Input>");
+                var msg = Console.ReadLine();
+                if (string.IsNullOrEmpty(msg)) continue;
+                pipe.Write("127.0.0.1",2001,msg);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
