b7d60e6aa917cd876bf91f7f1e16b43d32402161
nn4343 <gea01441@nifty.com>
2017-03-28 21:43:29 +0900

    作成中

---
+++ yajirushi/m1/Assets/scripts/Arrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow {
+
+    // ベース作成
+    public float m_line_width   = 0.5f;
+    public float m_arrow_height = 0.5f;
+    public float m_base_len     = 1.0f;
+    public int   m_joint_divnum = 10;
+
+    //
+    public class vertex
+    {
+        public int     index;  //インデックス
+        public Vector3 v;      //座標
+        public int     bone0;  //所属ボーン0
+        public int     bone1;  //所属ボーン1
+
+        public float   weight0; //ウエイト0
+        public float   weight1; //ウエイト1
+    }
+
+    public class 
+
+  
+
+}
