fb9af6029196017d7504aed78d3485a881794e31
nn4343 <gea01441@nifty.com>
2017-06-14 20:12:56 +0900

    no message

---
+++ excelapp/test5/test5/work.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using excelwork;
+
+class work
+{
+    ExcelControlWork m_ew;
+
+    public work()
+    {
+    }
+
+    public void START()
+    {
+        m_ew = ExcelControl.Create(@"C:\Users\gea01\Documents\project_underdevelop\excelapp\test5\out\a.xls"); 
+        m_ew.SetSheet(m_ew.GetActiveSheetIndex());
+
+        m_ew.SetVisible(true);
+    }
+
+    public void WRITE()
+    {
+        m_ew.SetObject(0,0,"0");
+        m_ew.Flush();
+    }
+
+    public void SAVE()
+    {
+        m_ew.Save();
+    }
+    public void CLOSE()
+    {
+        m_ew.Close();
+
+    }
+}
