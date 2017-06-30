a98cc764e31bdf56ae7c17f64508814ea94f9f1e
nn4343 <gea01441@nifty.com>
2017-06-30 21:36:59 +0900

    no message

---
+++ unity_form/UnityProject/Assets/Editor/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using formapp;
+
+public class Test : MonoBehaviour {
+
+    [MenuItem("Test/Test 1")]
+	static void TEST1() {
+		
+        var p = new Class1();
+        p.ShowMessageBox();
+	}
+    [MenuItem("Test/Test 2")]
+	static void TEST2() {
+		
+        var p = new Class1();
+        p.ShowSelected();
+	}
+
+    [MenuItem("Test/Test 3")]
+	static void TEST3() {
+		
+        var p = new Class1();
+        p.ShowForm();
+                   
+	}
+	
+}
---
+++ unity_form/formapp/formapp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace formapp
+{
+    public class Class1
+    {
+        public void ShowMessageBox()
+        {
+            MessageBox.Show("Test");
+        }
+
+        public void ShowSelected()
+        {
+            if (Selection.objects!=null && Selection.objects.Length>0)
+            {
+                MessageBox.Show(Selection.objects[0].name);
+            }
+        }
+
+        public void ShowForm()
+        {
+            System.Diagnostics.Debugger.Break();
+
+            var form = new Form1();
+            form.Show();
+            
+            form.dummy();
+        }
+    }
+}
